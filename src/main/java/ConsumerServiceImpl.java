59b02aedd36b8f324044cfff0c50c7abaa32ba74
dongqihust <1325756593@qq.com>
2015-09-15 21:05:03 +0800

    spring+activeMQ 单个机器实例

---
+++ src/main/java/ConsumerServiceImpl.java
@@ -0,0 +1,29 @@
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+
+/**
+ * Created by 95 on 2015/9/15.
+ */
+public class ConsumerServiceImpl {
+    private JmsTemplate jmsTemplate;
+
+    /**
+     * 接受消息
+     */
+    public void receive(Destination destination) {
+        TextMessage tm = (TextMessage) jmsTemplate.receive(destination);
+        try {
+            System.out.println("从队列" + destination.toString() + "收到了消息：\t"
+                    + tm.getText());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setJmsTemplate(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+}
---
+++ src/main/java/TopicMessageListener.java
@@ -0,0 +1,20 @@
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+/**
+ *和队列监听的代码一样。
+ */
+public class TopicMessageListener implements MessageListener {
+
+
+    public void onMessage(Message message) {
+        TextMessage tm = (TextMessage) message;
+        try {
+            System.out.println("TopicMessageListener \t" + tm.getText());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+}
